a05d93197077d478f45db40f2275f6fbe3550358
zalamon <lehoailinh2206@gmail.com>
2025-10-07 12:12:04 +0700

    update

--- apps/frontend/jest.config.js
+++ apps/frontend/jest.config.js
@@ -10,10 +10,7 @@ const customJestConfig = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
-  testMatch: [
-    '**/__tests__/**/*.[jt]s?(x)',
-    '**/?(*.)+(spec|test).[jt]s?(x)',
-  ],
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
   collectCoverageFrom: [
     'src/**/*.{js,jsx,ts,tsx}',
     '!src/**/*.d.ts',
@@ -31,4 +28,3 @@ const customJestConfig = {
 };
 
 module.exports = createJestConfig(customJestConfig);
-
