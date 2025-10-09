393c5fbd6f2ca982a0becae1be7a3ddd7bde043c
zalamon <lehoailinh2206@gmail.com>
2025-10-09 16:14:15 +0700

    u

--- apps/backend/src/shared/websocket/websocket.module.ts
+++ apps/backend/src/shared/websocket/websocket.module.ts
@@ -1,8 +1,8 @@
-import { Module, Global } from '@nestjs/common';
-import { WebSocketGateway } from './websocket.gateway';
-import { WebSocketService } from './websocket.service';
+import { Global, Module } from '@nestjs/common';
 import { ChatModule } from '../../modules/chat/chat.module';
 import { StreamsModule } from '../../modules/streams/streams.module';
+import { WebSocketGateway } from './websocket.gateway';
+import { WebSocketService } from './websocket.service';
 
 @Global()
 @Module({
